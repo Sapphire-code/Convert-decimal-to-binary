6073367c88d53e87e62358b15bb5a7f9a484bc3f
Sapphire-code <85494077+Sapphire-code@users.noreply.github.com>
2021-06-29 20:29:33 +0800

    Add files via upload

---
+++ convert decimal to binary.py
@@ -0,0 +1,11 @@
+vara = "x"
+print("x to close the program")
+decimal = input("Enter any number(s) in decimal form: ")
+if decimal == vara:
+                exit()
+
+else:
+                decimal = int(decimal);
+                print(decimal, "in binary = ", bin(decimal).replace('0b', ''))
+input("Type x to close the program: ")
+                
